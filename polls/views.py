109d1b5ee7925a56ecb015a4b1c880d0bdb9b87d
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-11 15:54:00 +0100

    feat: activate polls app + first template

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def dummy(request): 
+    return render(request, 'polls/vote.html', {})
