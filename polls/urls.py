18c814bc397cf26388597377458e14d124335257
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-11 17:12:31 +0100

    feat: handle form input and read a POST request

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.index, name='index'),
     path('dummy/', views.dummy, name='dummy'),
+    path('submit-vote/', views.submit_vote, name='submit_vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,41 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
 from django.shortcuts import render
 
+
 def index(request):
+    """
+    Hello world in our first app
+    """
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def dummy(request): 
+def dummy(request: HttpRequest): 
+    """
+    Dummy poll page, here user can try to vote.
+    """
+     
+    # render page for vote
     return render(request, 'polls/vote.html', {})
+
+def submit_vote(request: HttpRequest): 
+    """
+    Submit the vote and get the result
+    """
+
+    if request.method != "POST":
+        return HttpResponseBadRequest("Errore: il metodo deve essere post")
+    
+    if 'vote' not in request.POST:
+        return HttpResponseBadRequest("Errore: manca il voto")
+
+    options = {
+        '1': "Opzione 1", 
+        '2': "Opzione 2", 
+        '3': "Opzione 3"
+    }
+
+    choice = options.get(request.POST['vote'])
+
+    return render(request, 'polls/vote_confirm.html', {'choice': choice})
+    
+    
