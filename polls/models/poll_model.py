32c38c262e504f94fefd74fead1ebdc97fdd0cca
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-16 20:33:46 +0100

    refactor: add to string method to models
    Add to str method to models to improve console usability

--- polls/models/poll_model.py
+++ polls/models/poll_model.py
@@ -4,3 +4,10 @@ from django.db.models import CharField
 class PollModel(models.Model): 
     name: CharField = models.CharField(max_length=200)
     question: CharField = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str({
+            'id': self.id, 
+            'name': self.name,
+            'question': self.question
+        })
