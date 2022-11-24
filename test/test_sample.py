283a52b5b0b8c2f8c467b332a4fe15976bbf802e
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2022-11-24 17:00:19 +0100

    feat: added assertpy

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,7 +1,16 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
 
+import pytest
+from assertpy import assert_that
+from polls.dtos.poll_dto import PollDto
 
-def test_answer():
-    assert func(4) == 5
+from polls.services.poll_service import PollService
+# content of test_class.py
+class TestClass:
+    def test_one(self):
+        x = "this"  
+        assert_that(x).contains("h")
+    
+    @pytest.mark.django_db
+    def test_model(self):
+        poll: PollDto = PollService.create("test", "test", [{"key": "test", "value": "test"}])
+        assert poll.name == "test"
