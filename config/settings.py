426862649568256d6d97ff7ad42ed8a704565d63
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2022-11-26 18:06:55 +0100

    feat: created config module

---
+++ config/settings.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DEBUG_CONFIGURATION = os.getenv('DEBUG_CONFIGURATION', False) == 'True'
+
+ALLOWED_HOSTS_CONFIGURATION = os.getenv('ALLOWED_HOSTS_CONFIGURATION', []).split(',') if os.getenv('ALLOWED_HOSTS_CONFIGURATION', []) == [''] else []
