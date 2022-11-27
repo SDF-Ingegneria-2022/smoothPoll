0b25ad5d553eeea86cc9e8af7ba800f49233cc77
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-27 12:36:10 +0100

    feat!: URLs in italian

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,7 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.all_polls, name='all_polls'),
     path('dummy/', views.dummy, name='dummy'),
-    path('dummy/submit-vote/', views.submit_vote, name='submit_vote'),
-    path('dummy/results/', views.results, name='results'),
+
+    path('dummy/conferma-voto/', views.submit_vote, name='submit_vote'),
+    path('dummy/risultati/', views.results, name='results'),
 ]
