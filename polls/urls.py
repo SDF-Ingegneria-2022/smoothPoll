9039e1d878ba6756ab91b0ee6b5f04a4a119c09c
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-27 12:39:26 +0100

    fix: fix responsive issue on pages
    (fix mobile UI issue caused by margin)

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.all_polls, name='all_polls'),
     path('dummy/', views.dummy, name='dummy'),
-
     path('dummy/conferma-voto/', views.submit_vote, name='submit_vote'),
     path('dummy/risultati/', views.results, name='results'),
 ]
