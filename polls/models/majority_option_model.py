67759dfb65e9d00d204bc5b04a3ad6b54cd2d6c3
Denis <firelion94@hotmail.it>
2022-11-27 20:53:34 +0100

    feat: added majority vote result service and other fixes

--- polls/models/majority_option_model.py
+++ polls/models/majority_option_model.py
@@ -5,13 +5,13 @@ class MajorityOptionModel(models.Model):
     """Class which represents the majority vote choice and the
     rating for it"""
     
-    rating: models.PositiveSmallIntegerField = models.PositiveSmallIntegerField(default=1)
+    rating: models.PositiveSmallIntegerField = models.PositiveSmallIntegerField(default=None)
     """Majority vote rating value"""
 
     poll_option: models.ForeignKey = models.ForeignKey(POLL_OPTION_MODEL_NAME, on_delete=models.CASCADE)
     """Choosed pool option"""
 
-    poll_rating: models.ForeignKey = models.ForeignKey(MAJORITY_VOTE_MODEL_NAME, on_delete=models.CASCADE)
+    poll_vote: models.ForeignKey = models.ForeignKey(MAJORITY_VOTE_MODEL_NAME, on_delete=models.CASCADE)
     """Rating of a poll option"""
 
     def __str__(self):
--- polls/services/majority_vote_service.py
+++ polls/services/majority_vote_service.py
@@ -1,14 +1,12 @@
 from typing import List
 from django.core.exceptions import ObjectDoesNotExist
 from polls.exceptions.majority_number_of_ratings_not_valid import MajorityNumberOfRatingsNotValid
-#from polls.classes.majority_poll_result import MajorityPollResult
+from polls.classes.majority_poll_result import MajorityPollResult
 from polls.exceptions.poll_does_not_exist_exception import PollDoesNotExistException
 from polls.exceptions.poll_option_rating_unvalid_exception import PollOptionRatingUnvalidException
-from polls.exceptions.poll_option_unvalid_exception import PollOptionUnvalidException
 from polls.models.majority_option_model import MajorityOptionModel
 from polls.models.majority_vote_model import MajorityVoteModel
 from polls.models.poll_model import PollModel
-from polls.models.poll_option_model import PollOptionModel
 
 
 class MajorityVoteService:
@@ -20,9 +18,11 @@ class MajorityVoteService:
         Perform a vote on a majority vote poll
         Args:
             poll_id: the id of the poll.
-            rating_options: List of ratings assigned to an option [{'poll_choice_id': ..., 'rating_choosen': ...}, ...].
+            rating_options: List of dictionaries of ratings assigned to an option [{'poll_choice_id': ..., 'rating_choosen': ...}, ...].
         Raises:
-            PollNotValidCreationException: Is raised when the poll is not valid. When the input params not meet the requirements.
+            PollDoesNotExistException: execption raised when the poll selected is not present in the database
+            PollOptionRatingUnvalidException: exception raised when there are no rating choices in the options (None)
+            MajorityNumberOfRatingsNotValid: exception raised when the rating number is not between 1 and 5
         Returns:
             MajorityVoteModel: the majority vote model created.
         """
@@ -55,16 +55,16 @@ class MajorityVoteService:
 
         for num_ratings in rating_options:
             for rating_key, rating_value in num_ratings.items():
-                temp_majority_option: MajorityOptionModel = MajorityOptionModel
+                temp_majority_option: MajorityOptionModel = MajorityOptionModel()
                 temp_majority_option.poll_option = rating_key
                 temp_majority_option.rating = rating_value
-                temp_majority_option.poll_rating = vote.id  #Not sure about this
+                temp_majority_option.poll_vote = vote.id  #Not sure about this
                 temp_majority_option.save()
         
         return vote
 
-    #@staticmethod
-    #def calculate_result(poll_id: str) -> MajorityPollResult:
+    @staticmethod
+    def calculate_result(poll_id: str) -> MajorityPollResult:
         """
         Calculate result of a majority poll.
         """
@@ -74,4 +74,17 @@ class MajorityVoteService:
         except ObjectDoesNotExist:
             raise PollDoesNotExistException(f"Poll with id={poll_id} does not exist")
 
-        return MajorityPollResult(poll)
+        result: MajorityPollResult = MajorityPollResult(poll)
+
+        median: int = result.majority_count()
+
+        majority_vote_result_unsorted: list[tuple(int)] = []
+
+        for majority_option in result.poll_options:
+            majority_vote_result_unsorted.append(result.majority_count(majority_option, median))
+
+        majority_vote_result = result.print_result(majority_vote_result_unsorted)
+
+        return majority_vote_result
+
+# to remake better all the calculate result method
