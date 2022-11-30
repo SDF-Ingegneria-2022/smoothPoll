adc600f61b3c50200ffd093145dee10faf3d16d3
emanuelena49 <emanuelelena021099@gmail.com>
2022-11-30 12:08:04 +0100

    feat: insert a constant number of options in poll

--- polls/views/polls_create_view.py
+++ polls/views/polls_create_view.py
@@ -1,6 +1,6 @@
 from polls.classes.poll_form import PollForm
 from polls.models.poll_option_model import PollOptionModel
-from django.http import HttpRequest, HttpResponseRedirect
+from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -18,26 +18,56 @@ def create_poll_step_1_view(request: HttpRequest):
     if not form.is_valid():
         return HttpResponseRedirect(reverse('polls:create-poll-1'))
 
-    form.save()
-    #request.session['form-poll-id'] = form.id
+    poll = form.save()
+    request.session['form-poll-id'] = poll.id
 
-    # PollOptionModel(value="Opzione 1", poll_fk=form.id).save()
-    # PollOptionModel(value="Opzione 2", poll_fk=form.id).save()
+    # PollOptionModel(value="Opzione 1", poll_fk=poll.id).save()
+    # PollOptionModel(value="Opzione 2", poll_fk=poll.id).save()
 
     return HttpResponseRedirect(reverse('polls:create-poll-2'))
 
 def create_poll_step_2_view(request: HttpRequest):
     """View to handle step 2 of poll creation"""
 
-    # poll_id = request.session.get("form-poll-id")
-    # if poll_id is None:
-    #     HttpResponseRedirect(reverse('polls:create-poll'))
-
-    # actual_options = PollOptionModel.objects.filter(poll_fk=poll_id).all()
+    poll_id = request.session.get("form-poll-id")
+    if poll_id is None:
+        HttpResponseRedirect(reverse('polls:create-poll'))
 
     if request.method == "GET":
         return render(request, "polls/create_poll_step_2.html")
 
+    options = request.POST.getlist("options[]")
+    
+    if options is None:
+        # todo: render error: add at least n-options
+        # return HttpResponse(f"poche opzioni 1, {options}")
+        return HttpResponseRedirect(reverse('polls:create-poll-2'))
 
-    return HttpResponseRedirect(reverse('polls:all_polls', kwargs={'page':1}))
+    # remove white spaces before and at the end
+    def trim_str(s: str) -> str:
+        return s.strip()
+
+    options = map(trim_str, options)
+    # remove nulls
+    options = list(filter(None, options))
+    
+    if len(options)<2:
+        # todo: render error: add at least n-options
+        # return HttpResponse(f"poche opzioni 2, {options}")
+        return HttpResponseRedirect(reverse('polls:create-poll-2'))
+
+    if len(options)>10:
+        # todo: render error: not more than n-options
+        # return HttpResponse("troppe opzioni")
+        return HttpResponseRedirect(reverse('polls:create-poll-2'))
+
+    # todo: check for duplicates
+
+    # salva opzioni
+    for option in options:
+        PollOptionModel(value=option, poll_fk_id=poll_id).save()
+    
+    # return HttpResponse(str(options))
+    
+    return HttpResponseRedirect("%s?page=1&per_page=10" % reverse('polls:all_polls'))
 
