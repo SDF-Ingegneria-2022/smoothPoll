4bf884c7808a14f471f885dd459f15cc9c9166fb
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2022-12-01 18:58:42 +0100

    fix: fixed error handling

--- polls/views/dummy_view.py
+++ polls/views/dummy_view.py
@@ -167,7 +167,7 @@ def all_polls(request: HttpRequest):
         page_information: str = request.GET.get('page')
         per_page: int = int(request.GET.get('per_page'))
     except TypeError:
-        page = 1
+        page_information = '1'
         per_page = 10
     
     paginator: Paginator = PollService.get_paginated_polls(per_page)
