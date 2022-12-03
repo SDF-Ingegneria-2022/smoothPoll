dea7fa04ebcb1fa3e3262e02d827f1e46b894c44
emanuelena49 <emanuelelena021099@gmail.com>
2022-12-03 12:51:46 +0100

    fix: small fixes

--- polls/classes/majority_poll_result_data.py
+++ polls/classes/majority_poll_result_data.py
@@ -77,7 +77,7 @@ class MajorityPollResultData(object):
 
             res.append({
                 'value': value, 
-                'percentage': int(value*100), 
+                'percentage': math.floor(value*100), 
                 'style': f"background-color:{colors[i-1]}; color: {textcolors[i-1]}; ", 
                 'label': self.get_qualitative(i), 
             })
