f9f728c01f9d54f6e90c16e7fa786ab6e08332cb
Denis <firelion94@hotmail.it>
2022-12-15 12:18:16 +0100

    fix: better layout for popup control with javascript

--- polls/views/poll_delete_view.py
+++ polls/views/poll_delete_view.py
@@ -7,20 +7,22 @@ from polls.services.poll_service import PollService
 def poll_delete(request: HttpRequest, poll_id: int):
     """View method that deletes a poll"""
 
-    try:
-        # Retrieve poll
-        poll: PollModel = PollService.get_poll_by_id(poll_id)
-    except Exception:
-        raise Http404(f"Poll with id {poll_id} not found.")
+    if request.method == "POST":
+        try:
+            # Retrieve poll
+            poll: PollModel = PollService.get_poll_by_id(poll_id)
+        except Exception:
+            raise Http404(f"Poll with id {poll_id} not found.")
 
-    # poll_delete_service(poll_id)
-    # delete_success = True
+        # poll_delete_service(poll_id)
 
-    # return HttpResponseRedirect(reverse('polls:poll_delete', args=(poll_id, delete_success)))
+        # return HttpResponseRedirect(request.path_info, {
+        #                 'delete_success': True
+        #                 })
 
-    # return HttpResponseRedirect(request.path_info)
+        # return HttpResponseRedirect(reverse('polls:poll_delete', args=(poll_id, )))
 
-    return render(  request, 
+        return render(  request, 
                     'polls/all_polls.html', 
                     {
                     'delete_success': True
