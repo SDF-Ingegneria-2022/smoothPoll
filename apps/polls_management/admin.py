7e2741976725a85d8dfa0dcbc8690c32de16d361
emanuelena49 <emanuelelena021099@gmail.com>
2023-01-04 13:01:29 +0100

    feat: add admin panel to manage production

--- apps/polls_management/admin.py
+++ apps/polls_management/admin.py
@@ -1,3 +1,19 @@
+from .models.poll_model import PollModel
+from .models.poll_option_model import PollOptionModel
+
 from django.contrib import admin
 
-# Register your models here.
+# ----------------------------------------
+# Register your models here to make them
+# editable from admin panel
+
+class PollAdmin(admin.ModelAdmin):
+    pass
+
+class PollOptionAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PollModel, PollAdmin)
+admin.site.register(PollOptionModel, PollOptionAdmin)
+
+
