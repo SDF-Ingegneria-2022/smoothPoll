59f4594f7b4b0cb68d21b420f2d25fe4972be1fe
emanuelena49 <emanuelelena021099@gmail.com>
2023-01-04 13:07:20 +0100

    feat: add also vote models to admin

--- apps/polls_management/admin.py
+++ apps/polls_management/admin.py
@@ -1,11 +1,15 @@
 from .models.poll_model import PollModel
 from .models.poll_option_model import PollOptionModel
+from .models.vote_model import VoteModel
+from .models.majority_vote_model import MajorityVoteModel, MajorityJudgmentModel
 
 from django.contrib import admin
 
-# ----------------------------------------
 # Register your models here to make them
 # editable from admin panel
+
+# ----------------------------------------
+# polls management models
 
 class PollAdmin(admin.ModelAdmin):
     pass
@@ -16,4 +20,21 @@ class PollOptionAdmin(admin.ModelAdmin):
 admin.site.register(PollModel, PollAdmin)
 admin.site.register(PollOptionModel, PollOptionAdmin)
 
+# ----------------------------------------
+# vote models
 
+class SingleOptionVoteAdmin(admin.ModelAdmin):
+    pass
+
+class MajorityJudgmentVoteAdmin(admin.ModelAdmin):
+    pass
+
+class MajorityJudgmentJudgmentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(VoteModel, SingleOptionVoteAdmin)
+admin.site.register(MajorityVoteModel, MajorityJudgmentVoteAdmin)
+admin.site.register(MajorityJudgmentModel, MajorityJudgmentJudgmentAdmin)
+
+# TODO: give models more significative "__str__()" method 
+# (so admin panel is more readable)
