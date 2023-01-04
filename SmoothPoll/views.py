947f137079ad2de7b159398c8465ddb5c784b0f3
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2023-01-04 16:24:06 +0100

    feature: added version in home page

--- SmoothPoll/views.py
+++ SmoothPoll/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from apps.polls_management.models.poll_model import PollModel
+import configparser
 
 def home(request):
     """
     App home page
     """
     predefined_polls = PollModel.objects.filter(predefined=True)
+    config = configparser.ConfigParser()
+    config.read('.bumpversion.cfg')
+    version = config.get('bumpversion', 'current_version')
+    print(version)
     return render(request, "global/home.html",
-                  {"predefined_polls": predefined_polls})
+                  {"predefined_polls": predefined_polls, 
+                   "version": version})
 
 def error_404_view(request, exception):
     """
