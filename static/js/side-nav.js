471d28d684828ae03d50737a3c6cf5e022f0c2b2
emanuelena49 <emanuelelena021099@gmail.com>
2023-02-12 15:12:07 +0100

    feat: side nav structure responsive

--- static/js/side-nav.js
+++ static/js/side-nav.js
@@ -1,24 +1,32 @@
 /* 
-    Sidedrawer nav menu code. Say thanks to 
+    Sidedrawer nav menu code. Insipired by
     https://github.com/StartBootstrap/startbootstrap-simple-sidebar 
+    but heavily customized
 */
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
-    if (sidebarToggle) {
-
-        // Uncomment Below to persist sidebar toggle between refreshes
+    // get all boxes that toggle the sidebar
+    const sidebarTogglers = Array.from(
+        document.getElementsByClassName("sidebar-toggler")
+    );
+    
+    // On desktop I want to remember if sidebar is open or close, 
+    // On mobile I want it closed by default
+    const width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (width > 768 && sidebarTogglers.length > 0) {
         if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
             document.body.classList.toggle('sb-sidenav-toggled');
         }
+    }
 
-        sidebarToggle.addEventListener('click', event => {
+    // make sidebar toggle when one of the 
+    // togglers is clicked
+    sidebarTogglers.forEach((box) => {
+        box.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
-    }
-
+    }); 
 });
