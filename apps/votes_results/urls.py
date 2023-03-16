861c7a716a7c2ae6986a105433ef020b0ea1396e
emanuelena49 <emanuelelena021099@gmail.com>
2023-03-16 16:52:27 +0100

    feat: use term "scelta" in urls

--- apps/votes_results/urls.py
+++ apps/votes_results/urls.py
@@ -12,19 +12,26 @@ urlpatterns = [
     #path('<int:poll_id>/dettagli/', login_required(views.SingleOptionVoteView.as_view()), name='poll_details'),
 
     # generic vote and results (it will be redirect to main method)
-    path('<int:poll_id>/vota/', views.generic_vote_view, name='vote'), 
+    path('<int:poll_id>/scegli-o-giudica/', views.generic_vote_view, name='vote'), 
     path('<int:poll_id>/risultati/', views.generic_results_view, name='results'), 
 
     # vote-recap-results process for single option
-    path('<int:poll_id>/vota/scelta-singola/', views.SingleOptionVoteView.as_view(), name='single_option_vote'),
-    path('<int:poll_id>/riepilogo-scelta/scelta-singola/', views.single_option_recap_view, name='single_option_recap'),
+    path('<int:poll_id>/scelta-singola/', views.SingleOptionVoteView.as_view(), name='single_option_vote'),
+    path('<int:poll_id>/riepilogo-scelta-singola/', views.single_option_recap_view, name='single_option_recap'),
     path('<int:poll_id>/risultati/scelta-singola/', views.single_option_results_view, name='single_option_results'),
 
     # vote-recap-results process for majority judgment
-    path('<int:poll_id>/vota/giudizio-maggioritario/', views.MajorityJudgmentVoteView.as_view(), name='majority_judgment_vote'),
-    path('<int:poll_id>/riepilogo-scelta/giudizio-maggioritario/', views.majority_judgment_recap_view, name='majority_judgment_recap'),
+    path('<int:poll_id>/giudizio-maggioritario/', views.MajorityJudgmentVoteView.as_view(), name='majority_judgment_vote'),
+    path('<int:poll_id>/riepilogo-giudizio-maggioritario/', views.majority_judgment_recap_view, name='majority_judgment_recap'),
     path('<int:poll_id>/risultati/giudizio-maggioritario/', views.majority_judgment_results_view, name='majority_judgment_results'),
 
     # dummy majority judjment poll
     path('prova-giudizio-maggioritario/', views.MajorityJudgmentVoteView.as_view(), name='dummy_majority'),
+
+    # old urls (kept w redirect for retro-compatibility)
+    # path('<int:poll_id>/vota/', views.redirect_to_vote, name='vote_redirect_legacy'), 
+    # path('<int:poll_id>/vota/scelta-singola/', views.SingleOptionVoteView.as_view(), name='single_option_vote'),
+    # path('<int:poll_id>/riepilogo-scelta/scelta-singola/', views.single_option_recap_view, name='single_option_recap'),
+    # path('<int:poll_id>/vota/giudizio-maggioritario/', views.MajorityJudgmentVoteView.as_view(), name='majority_judgment_vote'),
+    # path('<int:poll_id>/riepilogo-scelta/giudizio-maggioritario/', views.majority_judgment_recap_view, name='majority_judgment_recap')
 ]
