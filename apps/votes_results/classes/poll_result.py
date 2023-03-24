3b225c8d580f0c89f7db54fb1cc3e757818b7331
jose <jose.dallatorre@hotmail.com>
2023-03-24 15:49:10 +0100

    feat: W.I.P. ui

--- apps/votes_results/classes/poll_result.py
+++ apps/votes_results/classes/poll_result.py
@@ -79,6 +79,7 @@ class PollResult:
 
 
         index =0 
+        aux_n_position=[]
         pos = 1 #temporary position
         n_pos = 1 #number of option on the same position
         for option in self.__memoized_result:
@@ -91,7 +92,22 @@ class PollResult:
                 pos += n_pos
                 option.position = pos
                 n_pos=1
+            aux_n_position.append([pos,option])
             index += 1
-            
 
-        return self.__memoized_result
+        print(aux_n_position)
+
+        
+        index = 0
+        n_position=[]
+        while index <len(aux_n_position)-1:
+            print(index)
+            print(aux_n_position[index][0],aux_n_position[index+1][0])
+            print(aux_n_position[index][1],aux_n_position[index+1][1])
+            if(aux_n_position[index][0]!=aux_n_position[index+1][0]):
+                n_position.append(aux_n_position[index])
+            index+=1
+        n_position.append(aux_n_position[-1])
+        return {"results":self.__memoized_result
+                #"positions":n_position
+                }
