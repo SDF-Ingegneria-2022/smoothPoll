55219ae1fdf78dedf9706c39b775d296be65a78e
emanuelena49 <emanuelelena021099@gmail.com>
2023-04-19 17:25:02 +0200

    test: prepare vote test config idea

--- test/view_level/vote_process/test_render_vote_generic.py
+++ test/view_level/vote_process/test_render_vote_generic.py
@@ -11,10 +11,15 @@ class TestRenderVoteGeneric(TestWithClient, abc.ABC):
     @pytest.fixture
     def create_poll(self, django_user_model):
         return create_single_option_polls(django_user_model, number_of_polls=1)[0]
+    
+    def _test_config_has_everything(self, test_config):
+        """Meta-test to ensure that the test config has everything it needs."""
+        assert_that(test_config).contains_key("vote_page_url")
 
-    def _test_render_vote_page(self, create_poll, test_config):
-        """Test rendering of vote page."""
-        assert_that(test_config).contains_key("prova")
+    def _test_render_vote_page(self, client, create_poll, test_config):
+        """Test rendering of vote page of an open poll"""
+        response = client.get(test_config["vote_page_url"])
+        assert_that(response.status_code).is_equal_to(200)
         
         
 
--- test/view_level/vote_process/test_render_vote_so.py
+++ test/view_level/vote_process/test_render_vote_so.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 import pytest
 from test.view_level.vote_process.test_render_vote_generic import TestRenderVoteGeneric
 
@@ -5,10 +6,17 @@ from test.view_level.vote_process.test_render_vote_generic import TestRenderVote
 class TestRenderVoteSO(TestRenderVoteGeneric):
 
     @pytest.fixture
-    def test_config(self):
-        return {"prova": "ciao"}
+    def test_config(self, create_poll):
+        return {
+            "vote_page_url": reverse(
+                'apps.votes_results:single_option_vote', args=(create_poll.id,))
+        }
     
     @pytest.mark.django_db
-    def test_render_vote_page(self, create_poll, test_config):
+    def test_config_has_everything(self, test_config):
+        self._test_config_has_everything(test_config)
+    
+    @pytest.mark.django_db
+    def test_render_vote_page(self, client, create_poll, test_config):
         """Test rendering of vote page."""
-        self._test_render_vote_page(create_poll, test_config)
+        self._test_render_vote_page(client, create_poll, test_config)
