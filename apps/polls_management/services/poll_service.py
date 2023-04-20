4f56046ca89a8b739ccf748a2a37be048a85ad96
dilettagiabardo <dilettagiabardo@gmail.com>
2023-04-20 11:36:55 +0200

    feat: create view and html

--- apps/polls_management/services/poll_service.py
+++ apps/polls_management/services/poll_service.py
@@ -3,7 +3,6 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from apps.polls_management.exceptions.paginator_page_size_exception import PaginatorPageSizeException
 from apps.polls_management.exceptions.poll_cannot_be_opened_exception import PollCannotBeOpenedException
-from apps.polls_management.exceptions.poll_is_close_exception import PollIsCloseException
 from apps.polls_management.exceptions.poll_is_open_exception import PollIsOpenException
 from apps.polls_management.exceptions.poll_not_valid_creation_exception import PollNotValidCreationException
 from apps.polls_management.exceptions.poll_does_not_exist_exception import PollDoesNotExistException
@@ -188,9 +187,9 @@ class PollService:
         
         if poll.is_closed():
             raise PollIsCloseException(f"Poll with id={id} is already close.")
-
-        poll.close_datetime = timezone.now()
-        poll.save()
-        return poll
+        else:
+            poll.is_closed=True
+            poll.save()
+            return poll
             
         
