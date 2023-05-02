055f35050f82ee98354786fca8020a0127b888e3
emanuelena49 <emanuelelena021099@gmail.com>
2023-05-02 17:04:45 +0200

    feat: closure not possible in certain cases + test

--- test/service_level/polls/test_poll_manual_close.py
+++ test/service_level/polls/test_poll_manual_close.py
@@ -2,7 +2,10 @@ from django.utils import timezone
 from typing import List, Tuple
 import pytest
 from assertpy import assert_that
+from apps.polls_management.exceptions.poll_cannot_be_closed_exception import PollCannotBeClosedException
+from apps.polls_management.exceptions.poll_is_close_exception import PollIsCloseException
 from apps.polls_management.models.poll_model import PollModel
+from apps.polls_management.services.poll_service import PollService
 
 from test.service_level.utils.create_polls_utils import create_single_option_polls
 
@@ -37,7 +40,7 @@ class TestPollManualClose:
 
         # set open and close date so polls are open now
         for k in create_polls.keys():
-            create_polls[k].open_datetime = timezone.now()
+            create_polls[k].open_datetime = timezone.now() - timezone.timedelta(days=1)
             create_polls[k].close_datetime = timezone.now() + timezone.timedelta(days=1)
             create_polls[k].save()
 
@@ -49,7 +52,7 @@ class TestPollManualClose:
     def test_non_open_polls_cannot_be_closed(self, create_polls):
 
 
-        # set open and close date so polls are open now
+        # set open and close date so polls are not yet open
         for k in create_polls.keys():
             create_polls[k].open_datetime = timezone.now() + timezone.timedelta(days=1)
             create_polls[k].close_datetime = timezone.now() + timezone.timedelta(days=2)
@@ -62,13 +65,84 @@ class TestPollManualClose:
 
     def test_closed_polls_cannot_be_closed(self, create_polls):
 
-        # set open and close date so polls are open now
+        # set open and close date so polls are already closed
         for k in create_polls.keys():
-            create_polls[k].open_datetime = timezone.now() - timezone.timedelta(days=1)
-            create_polls[k].close_datetime = timezone.now()
+            create_polls[k].open_datetime = timezone.now() - timezone.timedelta(days=2)
+            create_polls[k].close_datetime = timezone.now() - timezone.timedelta(days=1)
             create_polls[k].save()
 
         # check that some polls are closable and some not
         assert_that(create_polls['always_visible'].is_closable_now()).is_false()
         assert_that(create_polls['hidden_until_closed_for_all'].is_closable_now()).is_false()
         assert_that(create_polls['hidden_until_closed_for_voters'].is_closable_now()).is_false()
+
+        # ====== Close poll anytime ======
+    
+    @pytest.mark.django_db
+    def test_close_poll(self, create_polls):
+        """Test close poll"""
+        
+        # set open and close date so polls are open now
+        for k in create_polls.keys():
+
+            create_polls[k].open_datetime = timezone.now() - timezone.timedelta(days=1)
+            create_polls[k].close_datetime = timezone.now() + timezone.timedelta(days=1)
+            create_polls[k].save()
+
+        # check those polls can be closed
+        PollService.close_poll(create_polls["always_visible"].id)
+        create_polls["always_visible"].refresh_from_db()
+        assert_that(create_polls["always_visible"].is_closed()).is_true()
+
+        PollService.close_poll(create_polls["hidden_until_closed_for_all"].id)
+        create_polls["hidden_until_closed_for_all"].refresh_from_db()
+        assert_that(create_polls["hidden_until_closed_for_all"].is_closed()).is_true()
+
+        # check some polls instead cannot be closed
+        assert_that(PollService.close_poll) \
+            .raises(PollCannotBeClosedException) \
+            .when_called_with(create_polls["hidden_until_closed_for_voters"].id)
+
+    @pytest.mark.django_db
+    def test_close_poll_already_closed(self, create_polls):
+        """Test close poll, poll already closed"""
+
+        # set open and close date so polls are already closed
+        for k in create_polls.keys():
+            create_polls[k].open_datetime = timezone.now() - timezone.timedelta(days=2)
+            create_polls[k].close_datetime = timezone.now() - timezone.timedelta(days=1)
+            create_polls[k].save()
+
+        # check none of the polls can be closed again
+        assert_that(PollService.close_poll) \
+            .raises(PollIsCloseException) \
+            .when_called_with(id=create_polls["always_visible"].id)
+        assert_that(PollService.close_poll) \
+            .raises(PollIsCloseException) \
+            .when_called_with(id=create_polls["hidden_until_closed_for_all"].id)
+        assert_that(PollService.close_poll) \
+            .raises(PollIsCloseException) \
+            .when_called_with(id=create_polls["hidden_until_closed_for_voters"].id)
+
+    @pytest.mark.django_db
+    def test_close_poll_not_open(self, create_polls):
+        """Test close poll, poll that need to be opened"""
+        
+        # set open and close date so polls are not yet open
+        for k in create_polls.keys():
+            create_polls[k].open_datetime = timezone.now() + timezone.timedelta(days=1)
+            create_polls[k].close_datetime = timezone.now() + timezone.timedelta(days=2)
+            create_polls[k].save()
+
+        # check none of the polls can be closed before opening
+        assert_that(PollService.close_poll) \
+            .raises(PollCannotBeClosedException) \
+            .when_called_with(id=create_polls["always_visible"].id)
+        assert_that(PollService.close_poll) \
+            .raises(PollCannotBeClosedException) \
+            .when_called_with(id=create_polls["hidden_until_closed_for_all"].id)
+        assert_that(PollService.close_poll) \
+            .raises(PollCannotBeClosedException) \
+            .when_called_with(id=create_polls["hidden_until_closed_for_voters"].id)
+
+
