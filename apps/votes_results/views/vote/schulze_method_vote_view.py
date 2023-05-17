56f81e4162187d6d930ede8b530d25f6c1c27fcf
josedallatorre <jose.dallatorre@hotmail.com>
2023-05-17 17:07:49 +0200

    feat: cleaned session error in schulze view

--- apps/votes_results/views/vote/schulze_method_vote_view.py
+++ apps/votes_results/views/vote/schulze_method_vote_view.py
@@ -6,6 +6,7 @@ from apps.polls_management.exceptions.poll_does_not_exist_exception import PollD
 from apps.polls_management.models.poll_model import PollModel
 from apps.votes_results.exceptions.poll_option_unvalid_exception import PollOptionUnvalidException
 from apps.votes_results.classes.single_option_vote_counter import SingleOptionVoteCounter
+from apps.votes_results.services.schulze_method_vote_service import SchulzeMethodVoteService
 from apps.votes_results.services.single_option_vote_service import SingleOptionVoteService
 
 from django.http import Http404, HttpResponse  
@@ -15,11 +16,10 @@ from django.urls import reverse
 
 from apps.votes_results.views.vote.vote_view_schema import VoteViewSchema
 
-#da modificare con 'vote'
 REQUEST_VOTE = 'option_order'
 
-SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR = 'vote-submit-error'
-SESSION_SINGLE_OPTION_VOTE_ID = 'single-option-vote-id'
+SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR = 'vote-submit-error'
+SESSION_SCHULZE_METHOD_VOTE_ID = 'single-option-vote-id'
 SESSION_TOKEN_USED = 'token_used'
 
 
@@ -33,58 +33,45 @@ class SchulzeMethodVoteView(VoteViewSchema):
         return 'apps.votes_results:schulze_method_recap'
     
     def render_vote_form(self, request: HttpRequest) -> HttpResponse:
+        
+        # Get eventual error message and clean it
+        eventual_error = request.session.get(SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR)
+        if eventual_error is not None:
+            del request.session[SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR]
+        
+        # Render vote form (with eventual error message)
         return render(request, 
                     'votes_results/schulze_method_vote.html', 
                     { 
                         'poll': self.poll()
                     })
-        # Get eventual error message and clean it
-        """
-        eventual_error = request.session.get(SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR)
-        if eventual_error is not None:
-            del request.session[SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR]
-        
-        # Render vote form (with eventual error message)
-        return render(request, 
-                    'votes_results/single_option_vote.html', 
-                    { 
-                        'poll': self.poll(), 
-                        'error': eventual_error 
-                    })
-        """
 
     def perform_vote_or_redirect_to_form(self, request: HttpRequest) -> HttpResponse:
-        print(request.POST)
-        
-        # Check is passed any data.
-        """
         if REQUEST_VOTE not in request.POST:
-            request.session[SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR] = "Errore! Per confermare la scelta " \
+            request.session[SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR] = "Errore! Per confermare la scelta " \
                 + "devi esprimere una preferenza."
             return HttpResponseRedirect(reverse('apps.votes_results:schulze_method_vote', args=(self.poll().id,)))
 
         # Save vote preference in session
-        request.session[SESSION_SINGLE_OPTION_VOTE_ID] = request.POST[REQUEST_VOTE]
+        request.session[SESSION_SCHULZE_METHOD_VOTE_ID] = request.POST[REQUEST_VOTE]
         
         # Perform vote and handle missing vote or poll exception.
         try:
-            vote = SingleOptionVoteService.perform_vote(
-                self.poll().id, request.POST[REQUEST_VOTE])
+            vote = SchulzeMethodVoteService.perform_vote(
+                request.POST.getlist('option_order'),self.poll().id, )
         except PollOptionUnvalidException:
-            request.session[SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR] = "Errore! La scelte deve essere " \
+            request.session[SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR] = "Errore! La scelte deve essere " \
                 + "espressa tramite l'apposito form. Se continui a vedere questo " \
                 + "messaggio contatta gli sviluppatori."
             return HttpResponseRedirect(
-                reverse('apps.votes_results:single_option_vote', 
+                reverse('apps.votes_results:schulze_method_vote', 
                         args=(self.poll().id,)
                 ))
         except PollDoesNotExistException:
             raise Http404
 
-        # Clean eventual error session.
-        if request.session.get(SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR) is not None:
-            del request.session[SESSION_SINGLE_OPTION_VOTE_SUBMIT_ERROR]
-            """
+        if request.session.get(SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR) is not None:
+            del request.session[SESSION_SCHULZE_METHOD_VOTE_SUBMIT_ERROR]
         print(request.POST.getlist('option_order'))
         # Save user vote in session (so when I re-render with GET I have the vote).
         request.session['vote-submit-id'] = request.POST.getlist('option_order')
