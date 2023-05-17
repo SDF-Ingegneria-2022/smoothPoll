a94ef8b12a3da94650688bc003f67fbc754560ec
Denis <firelion94@hotmail.it>
2023-05-17 19:17:35 +0200

    feat: schulze results adapter (WIP)

---
+++ apps/votes_results/classes/schulze_results/schulze_results_adapter.py
@@ -0,0 +1,6 @@
+
+from apps.votes_results.classes.schulze_results.i_schulze_results import ISchulzeResults
+
+class SchulzeResultsAdapter(ISchulzeResults):
+    """Class used to manage data for a Schulze poll results"""
+    pass
