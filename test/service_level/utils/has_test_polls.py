013dbd83a5b6abc5e3e69b82158eaf5fa2693df1
Denis <firelion94@hotmail.it>
2023-05-18 14:05:26 +0200

    test: added test from wikipedia

--- test/service_level/utils/has_test_polls.py
+++ test/service_level/utils/has_test_polls.py
@@ -81,25 +81,47 @@ class HasTestPolls(abc.ABC):
 
         poll_test: PollModel = test_polls['poll_case_23']
 
-        op4 = poll_test.options()[4].id
-        op3 = poll_test.options()[3].id
-        op2 = poll_test.options()[2].id
-        op1 = poll_test.options()[1].id
-        op0 = poll_test.options()[0].id
+        op4 = poll_test.options()[4].id #E
+        op3 = poll_test.options()[3].id #D
+        op2 = poll_test.options()[2].id #C
+        op1 = poll_test.options()[1].id #B
+        op0 = poll_test.options()[0].id #A
 
-        input_user1: List[int] = [op4, op3, op2, op1, op0]
-        input_user2: List[int] = [op3, op2, op1, op0, op4]
-        input_user3: List[int] = [op2, op4, op3, op0, op1]
-        input_user4: List[int] = [op0, op1, op2, op3, op4]
+        # 5 people think A > C > B > E > D.
+        input_user1: List[int] = [op0, op2, op1, op4, op3]
+        # 5 people think A > D > E > C > B.
+        input_user2: List[int] = [op0, op3, op4, op2, op1]
+        # 8 people think B > E > D > A > C.
+        input_user3: List[int] = [op1, op4, op3, op0, op2]
+        # 3 people think C > A > B > E > D.
+        input_user4: List[int] = [op2, op0, op1, op4, op3]
+        # 7 people think C > A > E > B > D.
+        input_user5: List[int] = [op2, op0, op4, op1, op3]
+        # 2 people think C > B > A > D > E.
+        input_user6: List[int] = [op2, op1, op0, op3, op4]
+        # 7 people think D > C > E > B > A.
+        input_user7: List[int] = [op3, op2, op4, op1, op0]
+        # 8 people think E > B > A > D > C.
+        input_user8: List[int] = [op4, op1, op0, op3, op2]
 
         votes1: int = 5
-        votes2: int = 4
-        votes3: int = 2
-        votes4: int = 7
+        votes2: int = 5
+        votes3: int = 8
+        votes4: int = 3
+        votes5: int = 7
+        votes6: int = 2
+        votes7: int = 7
+        votes8: int = 8
 
         self.generate_votes_in_bulk(poll_test, input_user1, votes1)
         self.generate_votes_in_bulk(poll_test, input_user2, votes2)
         self.generate_votes_in_bulk(poll_test, input_user3, votes3)
         self.generate_votes_in_bulk(poll_test, input_user4, votes4)
+        self.generate_votes_in_bulk(poll_test, input_user5, votes5)
+        self.generate_votes_in_bulk(poll_test, input_user6, votes6)
+        self.generate_votes_in_bulk(poll_test, input_user7, votes7)
+        self.generate_votes_in_bulk(poll_test, input_user8, votes8)
+
+        # Expected results:  E > A > C > B > D
 
         return {'case1': poll_test}
