d76685f076d6e8a3a2b8e6f83a0ec944dcd68dde
Alexandru Dumitru <aaaalexdumitru@gmail.com>
2023-05-19 20:07:04 +0200

    fix: fixed bug in check

--- apps/votes_results/classes/vote_consistency/check_consistency_shulze.py
+++ apps/votes_results/classes/vote_consistency/check_consistency_shulze.py
@@ -9,23 +9,26 @@ class CheckConsistencyShulze:
         """Checks if the shulze vote key is consistent with the majority judgment votes.
 
         Args:
-            single_option_vote_key (str): Single option vote id.
+            shulze_vote_id (str): Shulze vote option vote id.
             mj_votes (List[dict]): List of dicts with mj keys and ratings. Example: [{'poll_choice_id': 1, 'rating': 3}, {'poll_choice_id': 2, 'rating': 2}]
 
         Returns:
-            bool: True if the single option vote key is consistent with the majority judgment votes, False otherwise.
+            bool: True if the Shulze option vote key is consistent with the majority judgment votes, False otherwise.
         """
         if shulze_vote_id != None:
             try:
                 vote = SchulzeMethodVoteService.get_vote_by_id(shulze_vote_id)
-                shulze_options = vote.get_order()
+                shulze_options: list = vote.get_order()
+                
                 for shulze_options_index in range(0, len(vote.get_order()) - 1):
-                    if list(filter(lambda x: x['poll_choice_id'] == shulze_options[shulze_options_index], mj_votes ))[0]['ratings'] <= list(filter(lambda x: x['poll_choice_id'] == shulze_options[shulze_options_index + 1], mj_votes ))[0]['ratings']:
-                        # TODO: fix bug
-                        return True
-            except Exception:
+                    previous_rating: list= list(filter(lambda x: x['poll_choice_id'] == int(shulze_options[shulze_options_index]), mj_votes ))[0]['rating']
+                    next_rating: list = list(filter(lambda x: x['poll_choice_id'] == int(shulze_options[shulze_options_index + 1]), mj_votes ))[0]['rating']
+                    
+                    if previous_rating <= next_rating:
+                        return False
+            except Exception as e:
                 # If the poll is not found, the vote is not consistent
-                return True
+                return False
         return True
         
         
